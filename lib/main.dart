1beccc0aaf8634ba830c670718f90337a629d9e1
Mubashir-Jaweed <mubashirmjjawed@gmail.com>
2025-05-21 22:55:31 +0500

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(scaffoldBackgroundColor: whiteColor,fontFamily: 'Poppins'),
       debugShowCheckedModeBanner: false,
       title: "Frido App",
-      home: Home(),
+      home: SplashView(),
     );
   }
 }
